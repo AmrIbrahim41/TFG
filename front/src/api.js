e9d3d6288489e2b79571aac606551ae3effb25a5
AmrIbrahim41 <Amrhima41@gmail.com>
2026-02-07 01:49:04 +0200

    api

--- front/src/api.js
+++ front/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // This points to your running Django server
-export const BASE_URL = 'http://127.0.0.1:8000';
+// export const BASE_URL = 'http://127.0.0.1:8000';
 
-// export const BASE_URL = 'https://TigerGym.pythonanywhere.com';
+export const BASE_URL = 'https://TigerGym.pythonanywhere.com';
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
   headers: {
