10e7f20de11c02b43f11cca6ebdc307344458c5b
AmrIbrahim41 <Amrhima41@gmail.com>
2026-02-06 11:19:36 +0200

    vercel --prod

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// 1. استيراد مكتبة التحليلات
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* 2. إضافة مكون التحليلات ليعمل في الخلفية */}
+    <Analytics />
   </StrictMode>,
 )
